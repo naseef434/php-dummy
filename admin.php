286f67d316b39b6a930b55701baed6588ff56966
naseef <naseefp.hin@gmail.com>
2022-06-20 19:28:30 +0530

    image

--- admin.php
+++ admin.php
@@ -1,13 +1,91 @@
 <!DOCTYPE html>
 <html>
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<title></title>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
-</form>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<br>
+<h3>Add Product </h3>
+<div class="container">
 
+		<form action="upload.php" method="post" enctype="multipart/form-data">
+	  	 <div class="form-group">
+    		<label for="exampleInputEmail1">Books Name</label>
+    		<input type="text" class="form-control"  name="book_name"  placeholder="Enter name">
+   
+ 		 </div>
+ 		  <div class="form-group">
+    		<label for="exampleInputEmail1">Author Name</label>
+    		<input type="text" class="form-control" name="author" placeholder="Enter Auther Name">
+   
+ 		 </div>
+ 		  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Discription</label>
+    <textarea class="form-control" name="discription" rows="3"></textarea>
+  </div>
+ 
+ 
+
+ 		  <div class="form-group">
+    		<label for="exampleInputEmail1">Price</label>
+    		<input type="text" class="form-control" name="price" placeholder="Enter Auther Name">
+   
+ 		 </div>
+
+
+ 		  <div class="form-group">
+    		<label for="exampleInputEmail1">Quantity</label>
+    		<input type="text" class="form-control" name="qty" placeholder="Enter Auther Name">
+   
+ 		 </div>
+	  Select image to upload:
+	  <input type="file" name="fileToUpload" id="fileToUpload">
+	  <input type="submit" value="Submit" name="submit">
+	
+	</form>
+</div>
+	  </div>
 </body>
 </html>
+
+
 
--- upload.php
+++ upload.php
@@ -1,4 +1,16 @@
+<?php  require 'connection.php' ?>
+
+
 <?php
+
+  $name  =  $_POST["book_name"];
+  $author  = $_POST["author"];
+  $discription  = $_POST["discription"];
+  $price  = $_POST["price"]; 
+  $qty = $_POST["qty"];
+  // $file = $_POST["fileToUpload"];
+  
+
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -16,26 +28,6 @@ if(isset($_POST["submit"])) {
   }
 }
 
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-
-  $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 1000000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
-
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
   echo "Sorry, your file was not uploaded.";
@@ -44,8 +36,36 @@ if ($uploadOk == 0) {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "name :".  $target_file;
     echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    $file =  htmlspecialchars( basename( $_FILES["fileToUpload"]["name"]));
+    $sql = "INSERT INTO books (book_name, author, discription,price,quantity,image)
+    VALUES ('$name', '$author', '$discription', '$price', '$qty','$file')";
+
+    if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
 }
+
+
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
